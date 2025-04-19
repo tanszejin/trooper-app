c6d1b5b5a1f41f3a6e83d2bd1f7d452e36c2c70f
tanszejin <tan.szejin@gmail.com>
2025-04-19 22:54:36 +0200

    added button styles

--- tddd27-app/src/components/Button.tsx
+++ tddd27-app/src/components/Button.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Button.css'
 
-const STYLES = ['btn--pressed--white', 'btn--pressed--clear']
+const STYLES = ['btn--pressed--white', 'btn--pressed--clear', 'btn--pressed--blue']
 const SIZES = ['btn--medium', 'btn--large'];
 
 interface Props {
