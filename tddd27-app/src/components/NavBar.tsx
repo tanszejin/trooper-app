3dab49d3e5eb434302687ff2f02d072a502a695a
tanszejin <tan.szejin@gmail.com>
2025-04-24 13:49:20 +0200

    add sign out button

--- tddd27-app/src/components/NavBar.tsx
+++ tddd27-app/src/components/NavBar.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
+import Button from "./Button";
 
 function NavBar() {
+  function signOut() {}
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/home" className="navbar-title">
           trooper
         </Link>
+        <div className="navbar-right-container">
+          <Button
+            onClick={signOut}
+            buttonColor="btn--clear"
+            buttonStyle="btn--lesspress"
+            buttonSize="btn--small"
+          >
+            Sign out
+          </Button>
+        </div>
       </div>
     </nav>
   );
