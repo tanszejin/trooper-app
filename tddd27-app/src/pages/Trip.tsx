edf9fd841b98b06507edf2e8f3ecc5c865c15021
tanszejin <tan.szejin@gmail.com>
2025-05-06 01:54:58 +0200

    add queries to backend

--- tddd27-app/src/pages/Trip.tsx
+++ tddd27-app/src/pages/Trip.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 import "./Trip.css";
 import Card from "../components/Card";
@@ -6,13 +6,41 @@ import Tasks from "../components/Tasks";
 import Reminders from "../components/Reminders";
 import Chat from "../components/Chat";
 import Itinerary from "../components/Itinerary";
+import { useParams } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase/firebase";
 
-function Trip() {  
-  return (
+
+function Trip() {
+  const { tripId } = useParams();
+  const [trip, setTrip] = useState<any>(null);
+
+  useEffect(() => {
+    console.log("showing trip: ", tripId);
+    getTripData();
+  }, []);
+
+  async function getTripData() {
+    try {
+      if (!tripId) {
+        throw new Error("no trip id in url");
+      }
+      const snapshot = await getDoc(doc(db, "trips", tripId));
+      if (!snapshot.exists()) {
+        throw new Error("trip does not exist");
+      }
+      setTrip(snapshot.data());
+    } catch (e) {
+      console.error(e);
+      setTrip(null);
+    }
+  }
+
+  if (trip) return (
     <>
-      <NavBar navbarColor="navbar--blue"/>
+      <NavBar navbarColor="navbar--blue" />
       <div className="trip-container">
-        <h3 className="trip-name">Trip name</h3>
+        <h3 className="trip-name">{trip.trip_name}</h3>
         <div className="trip-cards-container">
           <Card
             className="trip-image-card"
@@ -21,12 +49,12 @@ function Trip() {
             margin="0"
             padding={0}
           >
-            <img className="trip-image" src="/images/example.jpg" alt="image" />
+            <img className="trip-image" src={trip.image_url} alt="image" />
           </Card>
           <Itinerary></Itinerary>
         </div>
         <div className="trip-cards-container">
-          <Tasks></Tasks>
+          <Tasks tripId={tripId!}></Tasks>
           <Reminders></Reminders>
         </div>
         <div className="trip-cards-container">
