6c1213f5a89a6adabcdcaba64c17eaf972374ec9
tan sze jin <tan.szejin@gmail.com>
2025-08-14 23:03:47 +0800

    change to project with sg server

--- tddd27-app/src/firebase/firebase.tsx
+++ tddd27-app/src/firebase/firebase.tsx
@@ -3,13 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCHq8zJi00ORijH6ScwjxK31VjbL1IIHDU",
-  authDomain: "trooper-4c128.firebaseapp.com",
-  projectId: "trooper-4c128",
-  storageBucket: "trooper-4c128.firebasestorage.app",
-  messagingSenderId: "166509910603",
-  appId: "1:166509910603:web:0ed060150b15c8a85f8bce",
-  measurementId: "G-V3C9YXL6DB",
+  apiKey: "AIzaSyDUrmR-w4o-MwvQXVbAjNZgOnWinQeIArQ",
+  authDomain: "trooper-sg.firebaseapp.com",
+  projectId: "trooper-sg",
+  storageBucket: "trooper-sg.firebasestorage.app",
+  messagingSenderId: "570744451708",
+  appId: "1:570744451708:web:cac2d6ff9d22d43fae8ff9",
 };
 
 // Initialize Firebase
